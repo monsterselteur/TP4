b9636157adaa583e304e1dfb6a8330ea4d5d6387
Mamadou COULIBALY <mamadou.coulibaly@etu.univ-tours.fr>
2024-05-24 21:42:32 +0200

    Inter

--- connexion.py
+++ connexion.py
@@ -1,7 +1,7 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Float, ForeignKey,DateTime, Boolean
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine("mysql+mysqlconnector://root:root@localhost/aviation")
+engine = create_engine("mysql+mysqlconnector://root:Ddpassword@localhost:3308/aviation")
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from sqlalchemy import Column, Integer, Float, ForeignKey, Text, Date
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, and_, Boolean, Text, Date
 from connexion import Base, session, engine
-from Class import *
+from Class import Utilisateur,Compagnie,Avion,Siege,Pays,Reservation
 
+
+p = Pays.Pays(nom_P="France")
+p1 = Pays.Pays(nom_P="Alemagne")
+p2 = Pays.Pays(nom_P="Angletaire")
+p3 = Pays.Pays(nom_P="Afrique du Sud")
+session.add_all([p,p1,p2,p3])
+session.commit()
 Base.metadata.create_all(engine)
 
